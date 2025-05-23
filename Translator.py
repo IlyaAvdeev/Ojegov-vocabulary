5984adff9005e312d4dc9ade8b10209ec03032f3
Илья <ilya.s.avdeev@yandex.ru>
2025-05-23 09:42:30 +0300

    Rearranged code, preparing for several filters

--- Translator.py
+++ Translator.py
@@ -2,18 +2,18 @@ import csv
 import sys
 import os
 
-def transformToCSV(pathToFile, outputFolder, sourceEncoding) -> None:
-    with open(outputFolder + '/output.csv', 'w', newline='') as fw:
-        with open(pathToFile, 'rt', encoding=sourceEncoding) as fr:
-            for line in fr:
-                if len(line.strip()) > 0:
-                    fw.write(line)
-    fw.close()
-
+def dropEmptyLines(fw, pathToFile, sourceEncoding) -> None:
+    with open(pathToFile, 'rt', encoding=sourceEncoding) as fr:
+        for line in fr:
+            if len(line.strip()) > 0:
+                fw.write(line)
+    
 def runner() -> None:
-    workdir = os.environ.get('WORKDIR_NAME')
-    outputdir = os.environ.get('OUTPUTDIR_NAME')
-    transformToCSV(workdir + '/Ожегов_С._Толковый_словарь_русского_языка.txt', outputdir, 'utf-8')
-
+    workdir = os.environ.get('WORKDIR_PATH')
+    outputdir = os.environ.get('OUTPPUTDIR_PATH')
+    with open(outputdir + '/compressed_vocabulary.txt', 'w', newline='') as fw:
+        dropEmptyLines(workdir + '/Ожегов_С._Толковый_словарь_русского_языка.txt', outputdir, 'utf-8')
+    fw.close()
+    
 if __name__ == '__main__':
     runner()
